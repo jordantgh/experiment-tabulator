42f143660fde2856aa48c13d25313a9ace323bf4
s1349795 <s1349795@ed.ac.uk>
2023-01-12 07:29:45 +0000

    Whitespace edits

--- experiment-tabulator.r
+++ experiment-tabulator.r
@@ -18,23 +18,23 @@ rm(list = ls())
 ## Fix the column names (currently starts at Var2...)
 ##
 ## Account for the case (A; B; C)(a; b1, b2), i.e., when we want b1, b2 to
-## be applied B and C as they would in the case (B; C)(b1, b2). 
+## be applied B and C as they would in the case (B; C)(b1, b2).
 
 # Prompt
 ##
 ## The syntax is as follows:
 ##
-## Each parameter in the experiment (e.g., treatment, time, concentration, etc) 
+## Each parameter in the experiment (e.g., treatment, time, concentration, etc)
 ## should be enclosed within parentheses "(myparam1)(myparam2)(myparam3)(etc..)"
 ## within each parameter, you can have several values - these should be ";"
 ## separated like so "(param_val_1; param_val_2)". When computing combinations
-## of parameters, the script looks from left to right, so 
+## of parameters, the script looks from left to right, so
 ## in "(param1_val_1; param1_val_2)(param2_val_1; param2_val_2)", param1_val_1
 ## gets paired with param2_val_1, etc. Values can be further subdivided in to
-## comma separated subvalues. This is useful when different ranges of 
+## comma separated subvalues. This is useful when different ranges of
 ## conditions in one parameter apply to different values in another.
 ## For example, you might test two drugs using two different dosing schemes:
-## (DrugA, DrugB)(1mg/kg, 3mg/kg; 10mg/kg, 100mg/kg) will apply 1mg/kg and 
+## (DrugA, DrugB)(1mg/kg, 3mg/kg; 10mg/kg, 100mg/kg) will apply 1mg/kg and
 ## 3mg/kg for DrugA and 10mg/kg and 100mg/kg for DrugB. To add experimental
 ## replicates, just put a number anywhere between the brackets to indicate
 ## your n number. Sometimes you might have an experiment that mixes different
@@ -42,7 +42,7 @@ rm(list = ls())
 ## you can handle these mixed designs by separating them with a "+" symbol,
 ## e.g., 2(drugA)(1mg/kg, 3mg/kg) + 3(DrugB)(10mg/kg, 100mg/kg)
 
-s <- "(Control; TreatmentA; TreatmentB)(0; 1mg/kg, 3mg/kg; 10mg/kg, 100mg/kg)"
+s <- "(Control; KO)(SubCA, SubCB; SubKA, SubKB)(untreated, treated)"
 
 subexpts <- unlist(strsplit(s, split = "\\+"))
 
@@ -51,23 +51,33 @@ colnames(final) <- "Var1"
 for (s in subexpts) {
 
   # Extract the replicate index (discard all but the first number encountered)
-  n_replicates <- s %>% str_extract_all("\\)\\d+|\\d+\\(") %>% unlist() %>%
-    .[1] %>% str_replace_all("\\(|\\)", "")
+  n_replicates <- s %>%
+    str_extract_all("\\)\\d+|\\d+\\(") %>%
+    unlist() %>%
+    .[1] %>%
+    str_replace_all("\\(|\\)", "")
 
-  if (is.na(n_replicates)) { n_replicates <- 1 }
+  if (is.na(n_replicates)) {
+    n_replicates <- 1
+  }
 
   # Extract the multiplicands
-  contents <- s %>% str_extract_all("\\([^\\(\\)]*\\)") %>% unlist() %>%
-  str_replace_all("\\(|\\)", "")
+  contents <- s %>%
+    str_extract_all("\\([^\\(\\)]*\\)") %>%
+    unlist() %>%
+    str_replace_all("\\(|\\)", "")
 
   # Create a list of lists to store the values for each multiplicand
   values <- list()
 
   for (i in contents) {
     # Split the string around the semicolons, then commas, then trim whitespace
-    elements <- i %>% str_split(";") %>% unlist() %>%
-    lapply(function(x) unlist(str_split(x, ","))) %>% lapply(trimws)
-    
+    elements <- i %>%
+      str_split(";") %>%
+      unlist() %>%
+      lapply(function(x) unlist(str_split(x, ","))) %>%
+      lapply(trimws)
+
     # Nest the elements list within the values list
     values <- c(values, list(elements))
   }
@@ -80,20 +90,20 @@ for (s in subexpts) {
   # loop over the *inner* list indices
   for (i in 1:length(values[[1]])) {
     # apply expand.grid to the ith inner list elements
-    ## note lapply will generate a new list of elements, e.g., 
+    ## note lapply will generate a new list of elements, e.g.,
     ## newlist = { values[[1]][[2]], values[[2]][[1]], values[[3]][[2]] }
-    ## assuming i = 2 and the second list only had one element. Then 
+    ## assuming i = 2 and the second list only had one element. Then
     ## expand.grid is applied over these elements
     p <- do.call(expand.grid, c(list(1:n_replicates), lapply(
-            values,
-            function(x) {
-              if (i <= length(x)) {
-                x[[i]]
-              } else {
-                x[[1]]
-              }
-            }
-        )))
+      values,
+      function(x) {
+        if (i <= length(x)) {
+          x[[i]]
+        } else {
+          x[[1]]
+        }
+      }
+    )))
     result <- rbind(result, p) # we don't need to replicates column
   }
 
